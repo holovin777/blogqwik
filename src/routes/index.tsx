c2ab64fffee58a096f97f76eaea52ad43707ff49
Viktor Holovin <holovin@mail.com>
2024-02-04 22:59:20 +0100

    Add bio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,34 +2,58 @@ import { component$ } from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
 import Header from "~/components/header/header";
 import type UserProps from "~/interfaces/UserProps";
+import { marked } from "marked";
+
+export const useRepoDescription = routeLoader$(async () => {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/${import.meta.env.PUBLIC_GITHUB_LOGIN}/bio/master/README.md`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: import.meta.env.GITHUB_TOKEN,
+      },
+    }
+  );
+  const repoDescription = await res.text();
+  const html = marked.parse(repoDescription);
+  return html as string;
+});
 
 export const useUser = routeLoader$(async () => {
-  const res = await fetch(`https://${import.meta.env.PUBLIC_GITHUB_URL}/users/${import.meta.env.PUBLIC_GITHUB_LOGIN}`, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: import.meta.env.GITHUB_TOKEN,
-    },
-  });
+  const res = await fetch(
+    `https://${import.meta.env.PUBLIC_GITHUB_URL}/users/${
+      import.meta.env.PUBLIC_GITHUB_LOGIN
+    }`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: import.meta.env.GITHUB_TOKEN,
+      },
+    }
+  );
   return (await res.json()) as UserProps;
 });
 
 export default component$(() => {
   const userSignal = useUser();
+  const signalRepoDescription = useRepoDescription();
   return (
     <div>
-      {userSignal.value.bio &&
-        <Header title={userSignal.value.bio} />
-      }
+      {userSignal.value.bio && <Header title={userSignal.value.bio} />}
+      <div
+        class="md"
+        dangerouslySetInnerHTML={`${signalRepoDescription.value}`}
+      ></div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Welcome to my blog",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Blog description",
     },
   ],
 };
